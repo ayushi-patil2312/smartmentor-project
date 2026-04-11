ebc88100f9075b6cc350afee4d124f3404354f5d
Ayushi Patil <your-email@example.com>
2026-04-12 00:15:03 +0530

    fix env variable names

--- backend/db.py
+++ backend/db.py
@@ -6,6 +6,6 @@ def get_db_connection():
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT"))
+        database=os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE"),
+        port=int(os.getenv("MYSQLPORT") or 3306)
     )
