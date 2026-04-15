d5713520dcda07b31057a129b336a383d5724cf8
Ayushi Patil <your-email@example.com>
2026-04-15 15:19:32 +0530

    fix css path issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // important
-  build: {
-    outDir: 'dist',
-  },
+  base: "/",   // ✅ ADD THIS LINE
 })
